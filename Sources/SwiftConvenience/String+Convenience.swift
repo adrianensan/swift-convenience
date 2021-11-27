0d95c090e885312848969a4e49f83b433fdc10e8
Adrian Ensan <adrianensan@gmail.com>
2021-11-26 21:43:54 -0500

    String improvements

--- Sources/SwiftConvenience/String+Convenience.swift
+++ Sources/SwiftConvenience/String+Convenience.swift
@@ -1,10 +1,6 @@
 import Foundation
 
 public extension StringProtocol {
-  subscript(i: Int) -> Character {
-    self[index(startIndex, offsetBy: i)]
-  }
-  
   func distance(to element: Element) -> Int? {
     guard let index = firstIndex(of: element) else { return nil }
     return distance(from: startIndex, to: index)
@@ -13,26 +9,6 @@ public extension StringProtocol {
   func distance<S: StringProtocol>(to string: S) -> Int? {
     guard let range = range(of: string) else { return nil }
     return distance(from: startIndex, to: range.lowerBound)
-  }
-  
-  subscript(range: PartialRangeFrom<Int>) -> SubSequence {
-    self[index(startIndex, offsetBy: range.lowerBound)...]
-  }
-  
-  subscript(range: PartialRangeUpTo<Int>) -> SubSequence {
-    self[..<index(startIndex, offsetBy: range.upperBound)]
-  }
-  
-  subscript(range: PartialRangeThrough<Int>) -> SubSequence {
-    self[...index(startIndex, offsetBy: range.upperBound)]
-  }
-  
-  subscript(range: CountableClosedRange<Int>) -> SubSequence {
-    self[index(startIndex, offsetBy: range.lowerBound)...index(startIndex, offsetBy: range.upperBound)]
-  }
-  
-  subscript(range: CountableRange<Int>) -> SubSequence {
-    self[index(startIndex, offsetBy: range.lowerBound)..<index(startIndex, offsetBy: range.upperBound)]
   }
 }
 
--- Sources/SwiftConvenience/Trie.swift
+++ Sources/SwiftConvenience/Trie.swift
@@ -1,8 +1,23 @@
 import Foundation
 
+extension Character: Codable {
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    guard let character = try container.decode(String.self).first else {
+      throw DecodingError.dataCorruptedError(in: container, debugDescription: "Decoder expected a Character but found an empty string.")
+    }
+    self = character
+  }
+  
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(String(self))
+  }
+}
+
 public class TrieNode<T: Codable & Hashable>: Codable {
   public private(set) var value: [T]
-  public private(set) var map: [String: TrieNode]
+  public private(set) var map: [Character: TrieNode]
   
   public static func constructFrom(valuesMap: [String: T]) -> TrieNode {
     let root = TrieNode()
@@ -10,12 +25,12 @@ public class TrieNode<T: Codable & Hashable>: Codable {
     for (key, value) in valuesMap {
       var currentNode: TrieNode = root
       for character in key {
-        if let node = currentNode.map[String(character)] {
+        if let node = currentNode.map[character] {
           currentNode = node
           continue
         } else {
           let newNode = TrieNode()
-          currentNode.map[String(character)] = newNode
+          currentNode.map[character] = newNode
           currentNode = newNode
         }
       }
@@ -41,7 +56,17 @@ public class TrieNode<T: Codable & Hashable>: Codable {
   
   required public init(from decoder: Decoder) throws {
     let values = try decoder.container(keyedBy: TrieCodingKeys.self)
-    map = try values.decode([String: TrieNode].self, forKey: .map)
+    if let goodMap = try? values.decode([Character: TrieNode].self, forKey: .map) {
+      map = goodMap
+    } else {
+      let oldMap = try values.decode([String: TrieNode].self, forKey: .map)
+      map = [:]
+      for (key, val) in oldMap {
+        print(key)
+        map[Character(key)] = val
+      }
+      
+    }
     value = try values.decode([T].self, forKey: .value)
   }
   
@@ -56,12 +81,12 @@ public class TrieNode<T: Codable & Hashable>: Codable {
       let key = key.lowercased()
       var currentNode: TrieNode = self
       for character in key {
-        if let node = currentNode.map[String(character)] {
+        if let node = currentNode.map[character] {
           currentNode = node
           continue
         } else {
           let newNode = TrieNode()
-          currentNode.map[String(character)] = newNode
+          currentNode.map[character] = newNode
           currentNode = newNode
         }
       }
@@ -76,7 +101,7 @@ public class TrieNode<T: Codable & Hashable>: Codable {
   public func traverse(searchTerm: String) -> TrieNode? {
     var node: TrieNode? = self
     for character in searchTerm {
-      node = node?.map[String(character)]
+      node = node?.map[character]
     }
     return node
   }
